ef0ae1f671e8fdaded7dafc84964587b8786544c
DavidTrabuco <davidtrabuco88@gmail.com>
2025-04-01 17:32:21 -0300

    ATV_adivinhar

--- jogo/adivinhacao.js
+++ jogo/adivinhacao.js
@@ -1,19 +1,26 @@
+let tentativas = 0; 
+
 const gerador = Math.floor(Math.random()* 100) + 1 ;
+
 console.log(gerador);
 
 let palpite = parseInt(document.getElementById('palpite'));
-
+document.getElementById("tentativasRestantes").textContent = limiteTentativas;
 chute = document.getElementById("chute");
 dica = document.getElementById("dica");
 
     
 
 function adivinhacao(){
-
+    const limiteTentativas = 10;   
+    const contador = document.getElementById("tentativasRestantes"); 
+    const mensagem = document.getElementById("mensagem"); 
+    
     // Entrada de dados 
     const palpite = parseInt(document.getElementById("palpite").value);
+    const tentativasRestantes = limiteTentativas - tentativas;
     
-    let tentativas = 0; 
+     
     tentativas++;
 
     if (palpite < 1 || palpite > 100) {
@@ -23,6 +30,21 @@ function adivinhacao(){
     } else if (palpite > gerador) {
         document.getElementById('dica').innerText = "Esse valor está alto.";
     } else if (palpite === gerador){
-        document.getElementById('dica').innerText = `Parabéns! Você adivinhou o número ${gerador} .`;
+        document.getElementById('dica').innerText = `Parabéns! Você adivinhou o número ${gerador} em ${tentativas} tentativas!`;
+        
     }
+    if (tentativas >= limiteTentativas) {
+        document.getElementById("mensagem").textContent = 
+            "Game Over! Você esgotou todas as tentativas. O número era " + numeroSecreto;
+        document.getElementById("mensagem").style.color = "red";
+        
+    }
+
+    contador.textContent = tentativasRestantes;
+    
+    if (tentativasRestantes === 0) {
+        mensagem.textContent = "Game Over! Você esgotou todas as tentativas. O número era " + gerador;
+        
+    }
+    return tentativas;
 }
